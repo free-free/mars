660a87567d2682c49e0298391c0a05d3c0ec4887
jell <infinite.ft@gmail.com>
2017-01-09 21:46:32 +0800

    emit focusIn() signal in focusInEvent(QFocusEvent *). implement changeTitle(*) , changeGraphLegendName(*), changeAxisLabelName(*) method ,finally connecting them to the respective signal of parent class

--- marsplot.cpp
+++ marsplot.cpp
@@ -11,6 +11,16 @@
 MarsPlot::MarsPlot(QWidget * parent):QCustomPlot(parent)
 {
     /* hi,buddy you can fuck here */
+    QCPTextElement *title = new QCPTextElement(this, "Graph Title",
+          QFont("sans",14));
+    this->plotLayout()->insertRow(0);
+    this->plotLayout()->addElement(0, 0, title);
+    connect(this, &MarsPlot::legendDoubleClick,
+            this, &MarsPlot::changeGraphLegendName);
+    connect(this, &MarsPlot::axisDoubleClick,
+            this, &MarsPlot::changeAxisLabelName);
+    connect(title, &QCPTextElement::doubleClicked,
+            this, &MarsPlot::changeTitle);
 }
 
 
@@ -56,6 +66,71 @@ void MarsPlot::focusInEvent(QFocusEvent *event)
 {
     if(event->gotFocus())
     {
-
+        emit focusIn(this);
     }
 }
+
+/**
+ *@Desc: change title text of plotting
+ *@Args: QMouseEvent * event
+ *@Returns: None
+ */
+void MarsPlot::changeTitle(QMouseEvent * event)
+{
+
+    Q_UNUSED(event);
+    if(QCPTextElement * title = qobject_cast<QCPTextElement*>(sender()))
+    {
+        bool ok;
+        QString newTitle = QInputDialog::getText(this, tr("change title"),
+            tr("New Title:"),QLineEdit::Normal,title->text(),&ok);
+        if(ok)
+        {
+            title->setText(newTitle);
+            this->replot();
+        }
+    }
+}
+/**
+ *@Desc: change legend name of graph when doudle clicked on legend item
+ *@Args: QCPLegend * legend,QCPAbstractLegendItem * item
+ *@Return: None
+ */
+void MarsPlot::changeGraphLegendName(QCPLegend *legend, QCPAbstractLegendItem *item)
+{
+
+     Q_UNUSED(legend);
+     if (item)
+     {
+       bool ok;
+       QCPPlottableLegendItem *clickedItem = qobject_cast<QCPPlottableLegendItem*>(item);
+       QString newName = QInputDialog::getText(this, tr("change graph name"), tr("New Graph Name:"),
+                  QLineEdit::Normal, clickedItem->plottable()->name(), &ok);
+       if(ok)
+       {
+           clickedItem->plottable()->setName(newName);
+           this->replot();
+       }
+     }
+}
+
+/**
+ *@Desc: change axis label name when double click on axis
+ *@Args: QCPAxis * axis,QCPAxis::SelectablePart part
+ *@Return: None
+ */
+void MarsPlot::changeAxisLabelName(QCPAxis * axis,QCPAxis::SelectablePart part)
+{
+    if(part == QCPAxis::spAxisLabel)
+    {
+        bool ok;
+        QString newLabelName = QInputDialog::getText(this,tr("change axis label name"),
+                tr("New Axis Label Name:"),QLineEdit::Normal,axis->label(),&ok);
+        if(ok)
+        {
+            axis->setLabel(newLabelName);
+            this->replot();
+        }
+    }
+}
+
--- marsplot.h
+++ marsplot.h
@@ -7,11 +7,17 @@
 class MarsPlot : public QCustomPlot
 {
 
+Q_OBJECT
 public:
     explicit MarsPlot(QWidget * parent=0);
     ~MarsPlot();
    void wheelEvent (QWheelEvent *event);
    void focusInEvent(QFocusEvent*event);
+
+protected slots:
+   void changeGraphLegendName(QCPLegend *legend, QCPAbstractLegendItem *item);
+   void changeAxisLabelName(QCPAxis * axis,QCPAxis::SelectablePart);
+   void changeTitle(QMouseEvent * event);
 
 signals:
    void focusIn(MarsPlot * focusInObj);

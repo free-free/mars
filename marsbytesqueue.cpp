be2835f12f93a25ec61e00f9528d192308f6ab12
jell <infinite.ft@gmail.com>
2017-01-12 19:23:19 +0800

    add marsbytesqueue and helper file

---
+++ marsbytesqueue.cpp
@@ -0,0 +1,6 @@
+#include "marsbytesqueue.h"
+
+MarsBytesQueue::MarsBytesQueue()
+{
+
+}
---
+++ marsbytesqueue.h
@@ -0,0 +1,11 @@
+#ifndef MARSBYTESQUEUE_H
+#define MARSBYTESQUEUE_H
+
+
+class MarsBytesQueue : public QQueue<QByteArray>
+{
+public:
+    MarsBytesQueue();
+};
+
+#endif // MARSBYTESQUEUE_H

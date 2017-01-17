5f65118ca1cc3760621ead5dc849f0706e43d6cc
jell <infinite.ft@gmail.com>
2017-01-17 15:11:12 +0800

    include mars.h and create instance of Mars

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "mainwindow.h"
+#include "mars.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Mars w;
     w.show();
 
     return a.exec();
